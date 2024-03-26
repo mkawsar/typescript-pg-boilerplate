28b315380bf55fc2277dc2ec07d6f4f9e8053c13
mkawsar <kawsar.diu888@gmail.com>
2024-03-26 10:53:03 +0600

    created error and response interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,4 @@
+import { Response } from './response.interface';
+import { ApiErrorInterface } from './api.error.interface';
+
+export { ApiErrorInterface, Response };
---
+++ src/utils/general.ts
@@ -0,0 +1,18 @@
+import { Response } from '../interfaces';
+
+// Response for list
+const jsonAll = function<Res>(res: any, status: number, data: Res | Array<Res>, meta: object = {}) {
+    return res.status(status).json({
+        data: data,
+        meta: {...meta,},
+    });
+};
+
+// Response for details
+const jsonOne = function<Res>(res: any, status: number, data: Res) {
+    return res.status(status).json({
+        data: data
+    });
+};
+
+export { jsonAll, jsonOne };
